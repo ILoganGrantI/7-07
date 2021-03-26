527cd0ab4dbfd1eb108c31c21f6ecc148a6cabaa
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-26 13:40:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let input = 0
+let input1 = 0
+let mul = 0
+let ans = 0
+
+document.getElementById('button').addEventListener('click', click)
+
+function click () {
+input = document.getElementById('input').value
+input1 = document.getElementById('input1').value
+input = parseInt(input)
+input1= parseInt(input1)
+for (counter = 0; counter < input; counter = counter + 1) {
+ ans = ans + input1
+ ans = parseInt(ans)
+}
+document.getElementById('paragraph1').innerHTML = ans
+}
